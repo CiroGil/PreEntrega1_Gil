1d821eb3da07d7d535bcab8b1d13798c2651bfbd
CiroGil <ciro.rgil@gmail.com>
2023-07-03 20:17:05 -0300

    Orden del NavBar

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BsCartFill } from 'react-icons/bs';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 
 const CartWidget = () => {
-    return (
-        <div className="card">
-            <div className="card-body">
-                <h2 className="card-title"><BsCartFill /></h2>
-                <h3>0</h3>
-            </div>
+  return (
+        <div>
+          <Container>
+            <h2><BsCartFill /></h2>
+            <h3>0</h3>
+          </Container>
         </div>
-    )
+        )
 }
 
 export default CartWidget;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,16 +6,21 @@ import CartWidget from '../CartWidget/CartWidget';
 function NavBar() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="#home">Bahiana</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Lenceria</Nav.Link>
-            <Nav.Link href="#pricing">Mallas</Nav.Link>
-            <Nav.Link href="#pricing">Pijamas</Nav.Link>
-            <Nav.Link href="#pricing">Sexy</Nav.Link>
-            <Nav.Link href="#pricing"><CartWidget/></Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Lenceria</Nav.Link>
+              <Nav.Link href="#pricing">Mallas</Nav.Link>
+              <Nav.Link href="#pricing">Pijamas</Nav.Link>
+              <Nav.Link href="#pricing">Sexy</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#pricing"><CartWidget/></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
